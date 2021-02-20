8408ada48c631b34892e53c5b6aaeff20483b6da
Demo <ajendra1208@gmail.com>
2021-02-20 17:17:30 +0530

    first commit

---
+++ ESRFileEncryptor/ESRFileEncryptor/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RemoveDoubleQuotes = New System.Windows.Forms.Button()
+        Me.Encrypt = New System.Windows.Forms.Button()
+        Me.Decrypt = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Exit_Button = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RemoveDoubleQuotes
+        '
+        Me.RemoveDoubleQuotes.Location = New System.Drawing.Point(82, 6)
+        Me.RemoveDoubleQuotes.Name = "RemoveDoubleQuotes"
+        Me.RemoveDoubleQuotes.Size = New System.Drawing.Size(194, 30)
+        Me.RemoveDoubleQuotes.TabIndex = 0
+        Me.RemoveDoubleQuotes.Text = "Remove Double Quotes"
+        Me.RemoveDoubleQuotes.UseVisualStyleBackColor = True
+        Me.RemoveDoubleQuotes.Visible = False
+        '
+        'Encrypt
+        '
+        Me.Encrypt.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Encrypt.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Encrypt.Location = New System.Drawing.Point(40, 155)
+        Me.Encrypt.Name = "Encrypt"
+        Me.Encrypt.Size = New System.Drawing.Size(95, 35)
+        Me.Encrypt.TabIndex = 1
+        Me.Encrypt.Text = "Encrypt"
+        Me.Encrypt.UseVisualStyleBackColor = True
+        '
+        'Decrypt
+        '
+        Me.Decrypt.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Decrypt.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Decrypt.Location = New System.Drawing.Point(169, 155)
+        Me.Decrypt.Name = "Decrypt"
+        Me.Decrypt.Size = New System.Drawing.Size(95, 35)
+        Me.Decrypt.TabIndex = 2
+        Me.Decrypt.Text = "Decrypt"
+        Me.Decrypt.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Exit_Button
+        '
+        Me.Exit_Button.Location = New System.Drawing.Point(128, 264)
+        Me.Exit_Button.Name = "Exit_Button"
+        Me.Exit_Button.Size = New System.Drawing.Size(52, 35)
+        Me.Exit_Button.TabIndex = 3
+        Me.Exit_Button.Text = "Exit"
+        Me.Exit_Button.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(17, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(326, 18)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "GPG Encryption/Decryption Application"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(303, 318)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Exit_Button)
+        Me.Controls.Add(Me.Decrypt)
+        Me.Controls.Add(Me.Encrypt)
+        Me.Controls.Add(Me.RemoveDoubleQuotes)
+        Me.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.Text = "ESRFileEncryptor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RemoveDoubleQuotes As System.Windows.Forms.Button
+    Friend WithEvents Encrypt As System.Windows.Forms.Button
+    Friend WithEvents Decrypt As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Exit_Button As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ ESRFileEncryptor/ESRFileEncryptor/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.IO
+Imports Starksoft.Cryptography.OpenPGP  'After referencing Starksoft.Cryptography.OpenPGP.DLL
+Imports System.Configuration
+
+Public Class Form1
+
+#Region "member variables"
+
+    Private gpg As GnuPG = New GnuPG()
+    Dim fileName As String = "gpg.exe"
+    Private appPath As String = Path.Combine(Environment.CurrentDirectory, fileName) 'The path where gpg.exe is located
+    Private KeyName As String = ConfigurationManager.AppSettings.Get("PublicKey_Name")
+    Private passPhrase As String = ConfigurationManager.AppSettings.Get("SecretKeyName_PassPhrase")
+
+#End Region
+
+#Region "button events"
+    'This button event will remove the double quotes in the DAT file, or any text file.
+    Private Sub Remove_Double_Quotes(sender As System.Object, e As System.EventArgs) Handles RemoveDoubleQuotes.Click
+        Try
+            Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+            If result = DialogResult.OK Then
+                Dim FileLocation As String = OpenFileDialog1.FileName
+                Dim FileData As String = File.ReadAllText(FileLocation)
+
+                Dim NewFileData As String = FileData.Replace("""", "")
+                Dim fs As FileStream = File.Create(FileLocation) 'overwrite the existing the file
+                fs.Close()
+
+                Dim objWriter As New System.IO.StreamWriter(FileLocation)
+                objWriter.Write(NewFileData)
+                objWriter.Close()
+
+                MessageBox.Show("Please check your file, (Double Quotes removed).")
+            Else
+                MessageBox.Show("Please choose your .dat file")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Try
+        End Try
+    End Sub
+
+    'Here we handle the encryption of the DAT file
+    Private Sub Encrypt_file(sender As System.Object, e As System.EventArgs) Handles Encrypt.Click
+        Try
+            Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+            If result = DialogResult.OK Then
+                Dim SourceFileLocation As String = OpenFileDialog1.FileName
+
+                EncryptFile(KeyName, SourceFileLocation, SourceFileLocation + ".asc")
+                MessageBox.Show("File Encrypted Successfully with type (.asc) created with the same filename, in the same directory as source file.")
+            Else
+                MessageBox.Show("Please choose a file to encrypt")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Try
+        End Try
+    End Sub
+
+    'Here we handle the decryption of the encrypted DAT file
+    Private Sub Decrypt_file(sender As System.Object, e As System.EventArgs) Handles Decrypt.Click
+        Try
+            Dim result As DialogResult = OpenFileDialog1.ShowDialog()
+
+            If result = DialogResult.OK Then
+                Dim EncryptedFileLocation As String = OpenFileDialog1.FileName
+                Dim directoryPath As String = Path.GetDirectoryName(EncryptedFileLocation)
+                Dim filename = Path.GetFileNameWithoutExtension(System.IO.Path.GetFileName(OpenFileDialog1.FileName))
+
+                DecryptFile(EncryptedFileLocation, directoryPath + "\Decrypted_" + filename)
+                MessageBox.Show("File Decrypted Successfully, starting with (Decrypted_) followed by the same filename, in the same directory as Encrypted file.")
+            Else
+                MessageBox.Show("Please choose an encrypted file")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Try
+        End Try
+    End Sub
+
+    Private Sub Exit_Button_Click(sender As System.Object, e As System.EventArgs) Handles Exit_Button.Click
+        Me.Close()
+    End Sub
+#End Region
+
+#Region "functions"
+    Public Function EncryptFile(ByVal keyUserId As String, ByVal sourceFile As String, ByVal encryptedFile As String)
+        If String.IsNullOrEmpty(keyUserId) Then Throw New ArgumentException("Public key name is either empty or null", "keyUserId")
+        If String.IsNullOrEmpty(sourceFile) Then Throw New ArgumentException("sourceFile parameter is either empty or null", "sourceFile")
+        If String.IsNullOrEmpty(encryptedFile) Then Throw New ArgumentException("encryptedFile parameter is either empty or null", "encryptedFile")
+
+        Using sourceFileStream As Stream = New FileStream(sourceFile, FileMode.Open)
+            Using encryptedFileStream As Stream = New FileStream(encryptedFile, FileMode.Create)
+                gpg.BinaryPath = Path.GetDirectoryName(appPath)
+                gpg.Recipient = keyUserId
+                gpg.Encrypt(sourceFileStream, encryptedFileStream)
+            End Using
+        End Using
+        Return 0
+    End Function
+
+    Public Function DecryptFile(ByVal encryptedSourceFile As String, ByVal decryptedFile As String)
+        If String.IsNullOrEmpty(encryptedSourceFile) Then Throw New ArgumentException("encryptedSourceFile parameter is either empty or null", "encryptedSourceFile")
+        If String.IsNullOrEmpty(decryptedFile) Then Throw New ArgumentException("decryptedFile parameter is either empty or null", "decryptedFile")
+
+        Using encryptedSourceFileStream As FileStream = New FileStream(encryptedSourceFile, FileMode.Open)
+            encryptedSourceFileStream.Position = 0
+
+            Using decryptedFileStream As FileStream = New FileStream(decryptedFile, FileMode.Create)
+                gpg.BinaryPath = Path.GetDirectoryName(appPath)
+                gpg.Passphrase = passPhrase  'The security passphrase you created while creating the keys.
+                gpg.Decrypt(encryptedSourceFileStream, decryptedFileStream)
+            End Using
+        End Using
+        Return 0
+    End Function
+#End Region
+
+End Class
